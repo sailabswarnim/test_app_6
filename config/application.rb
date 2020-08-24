7a15a917822bf3f8b28a30fef94c82812fe99c57
sailabswarnim <sailab.endeavour@gmail.com>
2020-08-24 11:58:16 +0530

    Upto many to one assosciation in test_app

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module TestApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    def hello 
-    end
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
+  resources :users
   root 'pages#home'
 end
